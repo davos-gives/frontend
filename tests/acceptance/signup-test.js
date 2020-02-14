a4c59b0e0e6a843da3acf7ad784f73915fa38c41
ian knauer <ian.knauer@gmail.com>
2020-02-13 20:31:32 -0800

    Added percy

--- tests/acceptance/signup-test.js
+++ tests/acceptance/signup-test.js
@@ -1,13 +1,14 @@
 import { module, test } from 'qunit';
 import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
+import percySnapshot from '@percy/ember';
 
 module('Acceptance | login', function (hooks) {
   setupApplicationTest(hooks);
 
   test('visiting /singup shows the correct page', async function (assert) {
     await visit('/signup');
-
+    await percySnapshot('signup page');
     assert.equal(currentURL(), '/signup');
   });
 });
