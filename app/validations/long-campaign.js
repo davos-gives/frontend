d562a61499c1997086c5b2d9148c854490675454
ian knauer <ian.knauer@gmail.com>
2020-06-05 08:27:31 -0700

    remove extra bits from long campaign validation

--- app/validations/long-campaign.js
+++ app/validations/long-campaign.js
@@ -4,13 +4,6 @@ import {
 } from 'ember-changeset-validations/validators';
 
 export default {
-  // slug: [
-  //   validatePresence(true),
-  // ],
-  // amountEligableForReceipt: [
-  //   validatePresence(true),
-  //   validateNumber({ lt: 101 })
-  // ],
   description: [validatePresence(true)],
   name: [validatePresence(true)]
 };
