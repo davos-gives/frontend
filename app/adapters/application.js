339eb28795236038e959657bc2bd5d6d1aab2298
ian knauer <ian.knauer@gmail.com>
2020-06-25 21:31:05 -0700

    Add authorization header into application adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,18 @@
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class ApplicationAdapter extends JSONAPIAdapter {
   namespace = 'api/v1';
+
+  @service session;
+
+  @computed('session.data.authenticated.access_token')
+  get headers() {
+    let headers = {};
+    if (this.session.isAuthenticated) {
+      headers['Authorization'] = `${this.session.data.authenticated.access_token}`;
+    }
+    return headers;
+  }
 }
